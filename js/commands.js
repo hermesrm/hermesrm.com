c036782b01d7f1c87c746c16d6d6484f3c039e23
hermesrm <github.impose850@passinbox.com>
2024-03-06 15:02:17 +0100

    first commit

---
+++ js/commands.js
@@ -0,0 +1,93 @@
+var contraseña = "hermesrm";
+var linkedin = "https://www.linkedin.com/in/hermesrm/";
+var github = "https://github.com/hermesrm";
+var email = 'mailto:hermesrm@proton.me';
+
+acerca = [
+  "<br>",
+  "Hola, soy Hermes!👋",
+  "<br>",
+  "Me apasiona la tecnología y la informática.",
+  "Soy consultor administrativo y me estoy formando en el ramo de la Seguridad Informática, actualmente curso:",
+  "<br>",  
+    "🔹Certificado de Profesionalidad Nivel 3 en Seguridad Informática - IFCT0109.",
+    "🔹Certificado Profesional en Ciberseguridad de Google.",
+  "<br>",
+  "Estoy decidido a encontrar mi lugar en este sector, por ello:",
+  "<br>",
+  "🔸Soy proactivo.",
+  "🔸Tengo disponibilidad inmediata.",
+  "🔸Aprendo rápido y soy muy adaptable.",
+  "🔸Cuento con habilidades de comunicación.",
+  "🔸Capacidad de análisis y trabajo en equipo.",
+  "🔸Experiencia en diferentes sectores profesionales.",
+  "<br>",
+  "Estoy deseando colaborar contigo y aprender lo necesario para que alcancemos el éxito.",
+  "<br>",
+  "Saludos cordiales.",
+  "<br>"
+];
+
+experiencia = [
+  "<br>",
+  "En construcción...",
+  "<br>"
+];
+
+formacion = [
+  "<br>",
+  "En construcción...",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/hermesrm' + "</a>",
+  'github         <a href="' + github + '" target="_blank">github/hermesrm' + "</a>",
+  "<br>"
+];
+
+enigma = [
+  "<br>",
+  '<span class="command">sudo</span>           Úsalo solo si eres administrador.',
+  "<br>"
+];
+
+proyectos = [
+  "<br>",
+  "En construcción...",
+  "<br>"
+];
+
+ayuda = [
+  "<br>",
+  '<span class="command">acerca</span>         Acerca de mí.',
+  '<span class="command">experiencia</span>    Experiencia laboral.',
+  '<span class="command">formacion</span>      Formación y otros.',
+  '<span class="command">social</span>         Mis redes sociales',
+  '<span class="command">enigma</span>         Encuentra la contraseña.',
+  '<span class="command">proyectos</span>      Mis proyectos',
+  '<span class="command">historial</span>      Historial de comandos.',
+  '<span class="command">ayuda</span>          Ya sabes lo que hace. 😉',
+  '<span class="command">email</span>          Contacta y mándame un email.',
+  '<span class="command">limpia</span>         Limpia la terminal.',
+  '<span class="command">inicio</span>         Muestra la cabecera de inicio.',
+  "<br>",
+];
+
+inicio = [
+  '<span class="index">Espacio personal de Hermes Rodríguez Mohedano. Derechos reservados de </span>' + '<a href="' + github + '" target="_blank">Forrest Knight' + ".</a>",
+  '<br>',
+  '<br>',
+    "██╗  ██╗███████╗██████╗ ███╗   ███╗███████╗███████╗    ██████╗ ███╗   ███╗",
+    "██║  ██║██╔════╝██╔══██╗████╗ ████║██╔════╝██╔════╝    ██╔══██╗████╗ ████║",
+    "███████║█████╗  ██████╔╝██╔████╔██║█████╗  ███████╗    ██████╔╝██╔████╔██║",
+    "██╔══██║██╔══╝  ██╔══██╗██║╚██╔╝██║██╔══╝  ╚════██║    ██╔══██╗██║╚██╔╝██║",
+    "██║  ██║███████╗██║  ██║██║ ╚═╝ ██║███████╗███████║    ██║  ██║██║ ╚═╝ ██║",
+    "╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝╚══════╝    ╚═╝  ╚═╝╚═╝     ╚═╝ © 2024",
+  '<br>',
+  '<br>',
+  '<span class="color2">Bienvenido/a.</span>',
+  "<span class=\"color2\">Para ver la lista de comandos disponibles, escribe</span> <span class=\"command\">'ayuda'</span><span class=\"color2\">.</span>",
+  '<br>',
+];
---
+++ js/main.js
@@ -0,0 +1,189 @@
+var before = document.getElementById("before");
+var liner = document.getElementById("liner");
+var command = document.getElementById("typer"); 
+var textarea = document.getElementById("texter"); 
+var terminal = document.getElementById("terminal");
+
+var git = 0;
+var pw = false;
+let pwd = false;
+var commands = [];
+
+setTimeout(function() {
+  loopLines(inicio, "", 80);
+  textarea.focus();
+}, 100);
+
+window.addEventListener("keyup", enterKey);
+
+console.log(
+  "%c¡Has hackeado my contraseña!😠",
+  "color: #04ff00; font-weight: bold; font-size: 24px;"
+);
+console.log("%cContraseña: '" + contraseña + "' - Me pregunto ¿Que hará eso? 🤔", "color: grey");
+
+//init
+textarea.value = "";
+command.innerHTML = textarea.value;
+
+function enterKey(e) {
+  if (e.keyCode == 181) {
+    document.location.reload(true);
+  }
+  if (pw) {
+    let et = "*";
+    let w = textarea.value.length;
+    command.innerHTML = et.repeat(w);
+    if (textarea.value === contraseña) {
+      pwd = true;
+    }
+    if (pwd && e.keyCode == 13) {
+      loopLines(enigma, "color2 margin", 120);
+      command.innerHTML = "";
+      textarea.value = "";
+      pwd = false;
+      pw = false;
+      liner.classList.remove("contraseña");
+    } else if (e.keyCode == 13) {
+      addLine("Contraseña erronea", "error", 0);
+      command.innerHTML = "";
+      textarea.value = "";
+      pw = false;
+      liner.classList.remove("contraseña");
+    }
+  } else {
+    if (e.keyCode == 13) {
+      commands.push(command.innerHTML);
+      git = commands.length;
+      addLine("usuario@hermesrm.com:~$ " + command.innerHTML, "no-animation", 0);
+      commander(command.innerHTML.toLowerCase());
+      command.innerHTML = "";
+      textarea.value = "";
+    }
+    if (e.keyCode == 38 && git != 0) {
+      git -= 1;
+      textarea.value = commands[git];
+      command.innerHTML = textarea.value;
+    }
+    if (e.keyCode == 40 && git != commands.length) {
+      git += 1;
+      if (commands[git] === undefined) {
+        textarea.value = "";
+      } else {
+        textarea.value = commands[git];
+      }
+      command.innerHTML = textarea.value;
+    }
+  }
+}
+
+function commander(cmd) {
+  switch (cmd.toLowerCase()) {
+    case "ayuda":
+      loopLines(ayuda, "color2 margin", 80);
+      break;
+    case "acerca":
+      loopLines(acerca, "color2 margin", 80);
+      break;
+    case "experiencia":
+      loopLines(experiencia, "color2 margin", 80);
+      break;
+    case "formacion":
+      loopLines(formacion, "color2 margin", 80);
+      break;
+    case "sudo":
+      addLine("¡Oh no! Lo has logrado...", "color2", 80);
+      setTimeout(function() {
+        window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+      }, 1000); 
+      break;
+    case "social":
+      loopLines(social, "color2 margin", 80);
+      break;
+    case "enigma":
+      liner.classList.add("contraseña");
+      pw = true;
+      break;
+    case "proyectos":
+      loopLines(proyectos, "color2 margin", 80);
+      break;
+    case "contraseña":
+      addLine("<span class=\"inherit\"> ¿Enserio?¿Estás bromeando, verdad?¡Tendrás que intentarlo con más ganas!😂</span>", "error", 100);
+      break;
+    case "historial":
+      addLine("<br>", "", 0);
+      loopLines(commands, "color2", 80);
+      addLine("<br>", "command", 80 * commands.length + 50);
+      break;
+    case "email":
+      addLine('Opening mailto:<a href="mailto:hermesrm@pronto.me">hermesrm@proton.me</a>...', "color2", 80);
+      newTab(email);
+      break;
+    case "limpia":
+      setTimeout(function() {
+        terminal.innerHTML = '<a id="before"></a>';
+        before = document.getElementById("before");
+      }, 1);
+      break;
+    case "inicio":
+      loopLines(inicio, "", 80);
+      break;
+    // socials
+    case "youtube":
+      addLine("Opening YouTube...", "color2", 80);
+      newTab(youtube);
+      break;
+    case "twitter":
+      addLine("Opening Twitter...", "color2", 0);
+      newTab(twitter);
+      break;
+    case "linkedin":
+      addLine("Opening LinkedIn...", "color2", 0);
+      newTab(linkedin);
+      break;
+    case "instagram":
+      addLine("Opening Instagram...", "color2", 0);
+      newTab(instagram);
+      break;
+    case "github":
+      addLine("Opening GitHub...", "color2", 0);
+      newTab(github);
+      break;
+    default:
+      addLine("<span class=\"inherit\">Ese comando no está disponible. Si quieres ver la lista de comandos, escribe <span class=\"command\">'ayuda'</span>.</span>", "error", 100);
+      break;
+  }
+}
+
+function newTab(link) {
+  setTimeout(function() {
+    window.open(link, "_blank");
+  }, 500);
+}
+
+function addLine(text, style, time) {
+  var t = "";
+  for (let i = 0; i < text.length; i++) {
+    if (text.charAt(i) == " " && text.charAt(i + 1) == " ") {
+      t += "&nbsp;&nbsp;";
+      i++;
+    } else {
+      t += text.charAt(i);
+    }
+  }
+  setTimeout(function() {
+    var next = document.createElement("p");
+    next.innerHTML = t;
+    next.className = style;
+
+    before.parentNode.insertBefore(next, before);
+
+    window.scrollTo(0, document.body.offsetHeight);
+  }, time);
+}
+
+function loopLines(name, style, time) {
+  name.forEach(function(item, index) {
+    addLine(item, style, index * time);
+  });
+}
